a1343b73cce00628e9edc4ee7e102997dd3f836f
Dawid Janik <daw_janik@protonmail.com>
2019-11-04 19:14:38 +0100

    WIP #7: Contracted method for handle getMovies().

--- rating-data/src/main/java/pl/dsyou/movieapp/data/movie/MovieService.java
+++ rating-data/src/main/java/pl/dsyou/movieapp/data/movie/MovieService.java
@@ -1,14 +1,15 @@
 package pl.dsyou.movieapp.data.movie;
 
+import pl.dsyou.movieapp.data.movie.dto.MovieDetails;
 import pl.dsyou.movieapp.data.movie.dto.MovieRankAddition;
 import pl.dsyou.movieapp.data.movie.mongo.model.Movie;
 
 import java.util.List;
 
 public interface MovieService {
-    List<Movie> getMovies();
+    List<MovieDetails> getMovies();
 
-    void getMoviesRanks();
+    void getMoviesRanks(long id, MovieRankAddition movieRankAddition);
 
     void addMovieRank(String id, MovieRankAddition movieRankAddition);
 }
--- rating-data/src/main/java/pl/dsyou/movieapp/data/movie/MovieServiceImpl.java
+++ rating-data/src/main/java/pl/dsyou/movieapp/data/movie/MovieServiceImpl.java
@@ -3,25 +3,31 @@ package pl.dsyou.movieapp.data.movie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.dsyou.movieapp.data.movie.dto.MovieDetails;
 import pl.dsyou.movieapp.data.movie.dto.MovieRankAddition;
 import pl.dsyou.movieapp.data.movie.mongo.MovieRepository;
 import pl.dsyou.movieapp.data.movie.mongo.model.Movie;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MovieServiceImpl implements MovieService {
 
+    private final MovieMapper movieMapper;
     private final MovieRepository movieRepository;
 
     @Override
-    public List<Movie> getMovies() {
-        return movieRepository.findAll();
+    public List<MovieDetails> getMovies() {
+        return movieRepository.findAll()
+                .stream()
+                .map(movieMapper::toMovieDetails)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void getMoviesRanks() {
+    public void getMoviesRanks(long id, MovieRankAddition movieRankAddition) {
         // get only movies ranks
     }
 
