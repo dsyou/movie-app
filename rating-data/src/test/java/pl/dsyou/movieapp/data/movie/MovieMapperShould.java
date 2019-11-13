18cc01d0185443c4f7f212ad65f24b8e7dfa6289
Dawid Janik <daw_janik@protonmail.com>
2019-11-13 21:26:50 +0100

    Added test for mappings logic. Closed #20.

--- rating-data/src/test/java/pl/dsyou/movieapp/data/movie/MovieMapperShould.java
+++ rating-data/src/test/java/pl/dsyou/movieapp/data/movie/MovieMapperShould.java
@@ -1,13 +1,15 @@
 package pl.dsyou.movieapp.data.movie;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import pl.dsyou.movieapp.data.movie.dto.MovieDetails;
+import pl.dsyou.movieapp.data.movie.dto.MovieRegistration;
+import pl.dsyou.movieapp.data.movie.dto.MovieUpdate;
 import pl.dsyou.movieapp.data.movie.mongo.model.movie.Movie;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MovieMapperShould {
 
@@ -19,7 +21,7 @@ public class MovieMapperShould {
     private MovieMapper movieMapper = MovieMapper.INSTANCE;
 
     @Test
-    public void maToMovieDetailsFromMovie() throws ParseException {
+    public void mapToMovieDetailsFromMovie() throws ParseException {
         // given
         Movie movie = Movie.builder()
                 .title(title)
@@ -32,10 +34,47 @@ public class MovieMapperShould {
         MovieDetails movieDetails = movieMapper.toMovieDetails(movie);
 
         // then
-
         assertThat(movieDetails.getTitle()).isEqualTo(title);
         assertThat(movieDetails.getGenre()).isEqualTo(genre);
         assertThat(movieDetails.getProductionDate()).isEqualTo(productionDate);
         assertThat(movieDetails.getRank()).isEqualTo(rank);
     }
+
+    @Test
+    public void mapToMovieFromRegistration() throws ParseException {
+        // given
+        var movieRegistration = new MovieRegistration(title, genre, productionDate);
+
+        // when
+        Movie result = movieMapper.toMovie(movieRegistration);
+
+        // then
+        assertThat(result.getTitle()).isEqualTo(title);
+        assertThat(result.getGenre()).isEqualTo(genre);
+        assertThat(result.getProductionDate()).isEqualTo(new SimpleDateFormat("dd-MM-yyyy").parse(productionDate));
+        assertThat(result.getRank()).isEqualTo(0);
+    }
+
+    @Test
+    public void mapToMovieFromUpdate() throws ParseException {
+        // given
+        Movie movie = Movie.builder()
+                .title(title)
+                .genre(genre)
+                .productionDate(new SimpleDateFormat("dd-MM-yyyy").parse(productionDate))
+                .rank(rank)
+                .build();
+
+        var movieUpdate = new MovieUpdate("Ice Age", "Animation", "01-01-2002");
+
+        // when
+        movieMapper.toMovie(movieUpdate, movie);
+
+        // then
+        assertThat(movie.getTitle()).isEqualTo("Ice Age");
+        assertThat(movie.getGenre()).isEqualTo("Animation");
+        assertThat(movie.getProductionDate()).isEqualTo(new SimpleDateFormat("dd-MM-yyyy").parse("01-01-2002"));
+        assertThat(movie.getRank()).isEqualTo(rank);
+    }
+
 }
